1aa283efc771477fd2a004e02a7409e74413ef5d
Dinieva Rezeda <dinievarezeda@mail.ru>
2023-07-06 20:42:46 +0300

    contact form, sponsors

--- js.js
+++ js.js
@@ -7,6 +7,8 @@ const slidesCount = mainSlide.querySelectorAll('div').length;
 const navbar = document.querySelector('.header-navbar')
 const mobileMenu = document.querySelector('.mobile-menu')
 let targets = document.querySelectorAll('.anim')
+
+
 /* navbar */ 
 mobileMenu.addEventListener('click', ()=>{
     navbar.classList.toggle('active')
@@ -92,3 +94,40 @@ var swiper = new Swiper(".mySwiper", {
       el: ".swiper-pagination",
     },
   });
+
+  
+/* список спонсоров */
+let sponsorsTicker = document.querySelector('.sponsor-wrapper');
+let sponsors = [{name:'Ильфат Ильдарович', sum: '290.58 ₽'}, {name:'Анна Б.', sum: '500 ₽'}, {name:'Симона', sum: '50 € '},   {name:'Оливия', sum: '50 €'}, {name:'Лоренсо', sum: '300 €'}, {name:'Аноним', sum: '120 €'}/* ,{name:'Дима', sum: 500},{name:'Егор', sum: 600}, {name:'Жора', sum: 700}, {name:'Зак', sum: 800}, {name:'Игорь', sum: 900}, {name:'Карл', sum: 1000}, {name:'Лев', sum: 1100}, {name:'Макс', sum: 1200}, {name:'Николай', sum: 1300}, {name:'Олег', sum: 1400}, {name:'Петр', sum: 1500}, {name:'Роман', sum: 1600}, */ ];
+
+sponsors.forEach(sponsor => {
+
+    let card = document.createElement("div");
+    let ava = document.createElement("div");
+    let name = document.createElement("p");
+    let countBlock = document.createElement("div");
+    let sum = document.createElement("span");
+    let countBlockTitle = document.createElement("p");
+
+
+    card.classList.add("sponsor-card");
+    ava.classList.add("sponsor-avatar");
+    name.classList.add("sponsor-name");
+    countBlock.classList.add("count-block");
+    countBlockTitle.classList.add("count-block");
+    sum.classList.add("count-block-sum");
+
+    countBlockTitle.textContent="Сумма помощи";
+    name.textContent=sponsor.name;
+    sum.textContent=sponsor.sum;
+
+    sponsorsTicker.appendChild(card);
+    card.appendChild(ava);
+    card.appendChild(name);
+    card.appendChild(countBlock);
+    countBlock.appendChild(countBlockTitle);
+    countBlock.appendChild(sum);
+    
+})
+
+/* контактная форма */
